aa20f57959f1b9f48e6ebadab19d1d0dfb0f99df
Zhu-Detian <61304132+Zhu-Detian@users.noreply.github.com>
2020-03-17 19:22:08 +0800

    Homework4
    Load

---
+++ Homework4/GeneralList/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication
+{
+
+
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+    }
+    public void Foreach(Action<T> action)
+    {
+        Node<T> h = head;
+        while (h != null)
+        {
+            action(h.Data);
+            h = h.Next;
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            list.Foreach(x => Console.WriteLine(x));
+            int max = Int32.MinValue;
+            int min = Int32.MaxValue;
+            int sum = 0;
+            intlist.Foreach(s => { max = max < s ? s : max; });
+            intlist.Foreach(s => { min = min > s ? s : min; });
+            intlist.Foreach(s => { sum += s; });
+            Console.WriteLine($"Max:{max}");
+            Console.WriteLine($"Min:{min}");
+            Console.WriteLine($"Sum:{sum}");
+        }
+
+    }
+}
---
+++ Homework4/Homework4/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Homework4
+{
+    public delegate void AlarmHandler(object sender, ClockEventArgs args);
+    public delegate void TickHandler(object sender, ClockEventArgs args);
+
+    public class ClockEventArgs
+    {
+        public int currentTime { get; set; }
+        public ClockEventArgs(int t)
+        {
+            currentTime = t;
+        }
+    }
+
+    public class Clock
+    {
+        private int currentTime = 0;
+        private int[] alarmTimes;
+        public event TickHandler Tick;
+        public event AlarmHandler Alarm;
+        public void SetAlarmTime(params int[] times)
+        {
+            alarmTimes = times;
+            Array.Sort(alarmTimes);
+        }
+        public void StartTime(int cycle)
+        {
+            currentTime = 0;
+            int i = 0;
+            for (; currentTime <= cycle; currentTime++)
+            {
+                ClockEventArgs args = new ClockEventArgs(currentTime);
+                Tick(this, args);
+                if (i < alarmTimes.Length)
+                {
+                    if (alarmTimes[i] == currentTime)
+                    {
+                        Alarm(this, args);
+                    }
+                    if (alarmTimes[i] < currentTime) i++;
+                }
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Finished.");
+        }
+    }
+    public class  Example
+    {
+        public Clock aclock = new Clock();
+        public Example()
+        {
+            aclock.Tick += OnTick;
+            aclock.Alarm += OnAlarm;
+            void OnTick(object sender,ClockEventArgs args)
+            {
+                Console.WriteLine($"Tick {args.currentTime} times");
+            }
+            void OnAlarm(object sender,ClockEventArgs args)
+            {
+                Console.WriteLine($"Alarm!Current Time is : {args.currentTime} ");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example example1 = new Example();
+            example1.aclock.SetAlarmTime(4,9,15);
+            example1.aclock.StartTime(20);
+        }
+    }
+}
