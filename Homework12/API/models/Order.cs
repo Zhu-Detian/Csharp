15950e60023574d4615d2498ad19d688d967fafb
Zhu-Detian <61304132+Zhu-Detian@users.noreply.github.com>
2020-05-23 18:17:31 +0800

    Homework12
    Homework12

---
+++ Homework12/API/models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderApi.models
+{
+    public class Order
+    {
+        [Key]
+        public string OrderId { get; set; }
+        [Required]
+        public string ClientName { get; set; }
+        public string CreateTime { get; set; }
+        public double Total { get; set; }
+        public List<OrderItem> Items { get; set; }
+
+    }
+}
---
+++ Homework12/API/models/OrderContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderApi.models
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated(); 
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+    }
+}
